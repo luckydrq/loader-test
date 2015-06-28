ebcc35c8bfc4b8f5ac0fdd9cfa5bbfc51fa50b02
xuezu <ruoqi.drq@alibaba-inc.com>
2015-06-28 16:05:06 +0800

    update logic

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ AMD与CMD的一个简单demo
 
 ```base
 $ npm install
-$ node --harmony index.js
+$ npm start
 
 // console输出
 server start at 3000
@@ -29,8 +29,8 @@ main hello
 
 ## AMD
 ```
-mod1 required
 mod2 required
+mod1 required
 main required
 mod1 hello
 mod2 hello
@@ -39,4 +39,12 @@ main hello
 
 ## 结论
 
-从结果看来，`CMD`和`AMD`最大的区别在于模块的执行顺序，而不是模块的加载顺序（都是异步请求）。`CMD`是懒执行的，执行流从主模块开始，`require`到依赖模块时再去执行依赖模块的`factory`。而`AMD`是在执行主模块`factory`之前，先执行所有依赖模块的`factory`。
+`CMD`和`AMD`的区别：
+
+1. 模块执行机制差别
+
+首先，要清楚模块执行机制和模块加载机制的概念。模块加载机制是指浏览器请求模块的方式，这里两者都是异步请求，因此没有区别。而模块执行机制是指`factory`的执行顺序。`CMD`是懒执行的，即执行流从主模块`factory`开始，如果`require`到某个依赖模块，则再去执行该依赖模块的`factory`。而`AMD`是在执行主模块`factory`之前，先执行所有依赖模块的`factory`。
+
+2. 依赖模块的执行顺序差别
+
+在上一条结论里，由于`CMD`是懒执行的，因此执行顺序始终是一致的。而`AMD`里各自的依赖模块执行顺序是无序的，即浏览器请求到模块时立即执行`factory`。在demo的代码里故意延迟加载`mod1.js`，从输出结果可以看到mod2先执行。
--- index.js
+++ index.js
@@ -5,16 +5,37 @@ var app = koa();
 var fs = require('fs');
 var path = require('path');
 
-app.use(require('koa-static')(__dirname + '/public'));
-app.use(function* (){
+app.use(function* (next){
+  var RE_JS_PATH = /\/(.+?\.js)$/;
+
   if (this.path === '/index') {
     this.body = fs.createReadStream(path.join(__dirname, 'views/index.html'));
     this.type = 'html';
   } else if (this.path === '/index2') {
     this.body = fs.createReadStream(path.join(__dirname, 'views/index2.html'));
     this.type = 'html';
+  } else if (RE_JS_PATH.test(this.path)) {
+    var js = RE_JS_PATH.exec(this.path)[1];
+    var jpath = path.join(__dirname, 'public', js);
+
+    this.type = 'js';
+
+    // 对mod1.js延迟100ms响应
+    if (js === 'mod1.js') {
+      this.body = yield thunk(jpath);
+    } else {
+      this.body = fs.createReadStream(jpath);
+    }
   }
 });
+
+function thunk(js) {
+  return function(callback) {
+    setTimeout(function (){
+      callback(null, fs.createReadStream(js));
+    }, 100);
+  };
+}
 
 app.listen(3000, function() {
   console.log('server start at 3000');
